dff4adb32f9672e7e964cf6dff95c77b31f6fd7d
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2023-12-15 20:15:55 +0100

    ...

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from userauths import views
+
+app_name = "userauths"
 
 urlpatterns = [
-    
+    path('user/sign-up/', views.register_view, name= "sign-up"),
+    path('user/sign-in/', views.login_view, name="sign-in"),
+    path('logout', views.logout_view, name="logout"),
+    path('lockscreen', views.lock_screen_view, name='lockscreen'),
 ]
+
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.conf import settings
+from django.contrib.auth import logout
+from userauths.models import User
+from django.conf import settings
 
-# Create your views here.
+
+
+
+
+def register_view(request):
+
+    form = UserRegisterForm()
+    if request.method == "POST":
+        address = request.POST['address']
+        username = request.POST['username']
+        form = UserRegisterForm(request.POST or None)
+        
+        if form.is_valid():
+            
+            new_user = form.save()
+            
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, account created successfully")
+            new_user = authenticate(username=form.cleaned_data['email'],
+                                    password=form.cleaned_data['password1']
+            )
+            # send_activation_email(new_user,request)
+            login(request, new_user)
+            return redirect("core:dashboard")
+    context = {
+        'form': form,
+    }
+    return render(request, 'userauths/sign-up.html', context)
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("core:index")
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Successfully logged in.")
+                return redirect("core:dashboard")
+            else:
+                messages.warning(request, "Invalid credentials, create an account.")
+        except:
+            messages.warning(request, f"User does not exist")
+
+
+    return render(request, 'userauths/sign-in.html' )
+
+
+def logout_view(request):
+    logout(request)
+    # messages.success(request, "User successfully logged out.")
+    return redirect("core:index")
+
+def lock_screen_view(request):
+    logout(request)
+    return redirect("userauths:sign-in")
