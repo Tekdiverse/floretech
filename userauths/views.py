92d7de18e13a8fc9174667f44dc913ddf10a7d1d
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2024-01-04 21:51:14 +0100

    ...

--- userauths/views.py
+++ userauths/views.py
@@ -314,7 +314,7 @@ def perform_daily_task():
     for transaction in transactions:
         # Calculate the time difference between the current time and the transaction timestamp
         time_difference = current_time - transaction.timestamp
-        if int(transaction.interval_count) <= int(transaction.convert_description_to_days()) and not transaction.plan_interval_processed:
+        if int(transaction.interval_count) < int(transaction.convert_description_to_days()) and not transaction.plan_interval_processed:
             if (
                 (transaction.interval == 'hourly' and time_difference.seconds >= 30) or
                 (transaction.interval == 'daily' and time_difference.days >= 1) or
