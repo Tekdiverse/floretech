e8ba3e0e6e2871745cbecb00fb04911aa9f09a5d
Chijioke Divine <108485884+ChijiokeDivine@users.noreply.github.com>
2024-02-23 18:49:41 +0100

    ...

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -54,7 +54,9 @@ $(document).ready(function(){
                     // Login success
                     
                     // Redirect to the user profile page
-                    window.location.href = '/';  // Update with the actual URL
+                    $('.dissapear-content').html("")
+                    $('#passwordCheckmark').html('<div style="display: flex; align-items: center; justify-content: center;"><svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 12L9 18L21 6" stroke="rgba(0,0,0,0.95)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>')
+                    $('#passwordMessage').html('<p>An email has been sent to your account to reset your password.</p>')
                 } else {
                     // Login failure
                     $("#ForgotPasswordErrorMessage").text(response.message);
